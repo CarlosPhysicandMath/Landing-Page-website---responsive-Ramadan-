4e5819ba2788f5bbeb9650975b956e8a0ae4ccda
Carlos Henrique <henrique5802@gmail.com>
2022-07-26 11:42:06 -0300

    add Js

---
+++ Landing Page 4 - Website Ramadan/exemplodesite8.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.toggle');
+		menuToggle.onclick = function() {
+			menuToggle.classList.toggle('active'); }
+		const navigation = document.querySelector('.navigation');
+		menuToggle.onclick = function(){
+			navigation.classList.toggle('active');
+		} 
